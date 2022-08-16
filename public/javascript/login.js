926c0a9283a000b06fb5d65d666ef29305347fcd
lundbmp <m.lundberg008@gmail.com>
2022-08-15 19:34:30 -0500

    added login functionality

---
+++ public/javascript/login.js
@@ -0,0 +1,26 @@
+async function loginFormHandler() {
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    // check response status
+    if(response.ok) {
+        console.log("You are logged in...");
+        document.location.replace("/");
+    } else {
+        alert("login failed");
+    }
+  }
+}
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
